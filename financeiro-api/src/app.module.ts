fa3c40650cda9bcf4e8fd3bbf84cc4b5a491e88b
João Otávio <joaotavioos@outlook.com>
2022-07-29 20:01:05 -0300

    feat: add docker and modules configuration ✨

--- financeiro-api/src/app.module.ts
+++ financeiro-api/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Transaction } from './transactions/entities/transaction.entity';
+import { TransactionsModule } from './transactions/transactions.module';
+import { AccountModule } from './account/account.module';
+import { Account } from './account/entities/account.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: process.env.DB_CONNECTION as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      models: [Transaction, Account],
+      autoLoadModels: true,
+      synchronize: true,
+      sync: {
+        alter: true,
+      },
+    }),
+    TransactionsModule,
+    AccountModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
